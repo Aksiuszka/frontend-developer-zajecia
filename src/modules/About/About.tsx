6d13e248e5886db779cd5dcf5463fd79dabaf1f6
Anna Kawecka <aakawecka89@gmail.com>
2023-11-18 11:17:20 +0100

    Feat/about page finished

--- src/modules/About/About.tsx
+++ src/modules/About/About.tsx
@@ -1,19 +1,37 @@
 import Ball from "../../assets/Ball.svg";
 import { Flex, AnimatedContainer, Card } from "../../shared/style";
-import { Grid, Paper } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 import { useAnimation } from "../../shared/hooks/useAnimation";
+import AboutImg from "../../assets/About.svg";
 
 export default function About() {
-    const isActive = useAnimation();
+  const isActive = useAnimation();
   return (
     <Flex>
       <AnimatedContainer className={isActive ? "active" : ""}>
         <img src={Ball} alt="logo" />
       </AnimatedContainer>
       <Card className={isActive ? "active" : ""}>
-        <Grid>
-          <Paper>xs</Paper>
-          <Paper>xs</Paper>
+        <Grid container spacing={1} marginInline={6}>
+          <Grid item xs={12} lg={6}>
+            <img src={AboutImg} alt="about" />
+          </Grid>
+          <Grid item xs={12} lg={6}>
+            <Typography
+              style={{
+                fontFamily: "Poppins",
+                fontSize: "1rem",
+              }}
+            >
+              Lorem ipsum dolor sit amet, consectetur adipisici elit, sed
+              eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim
+              ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+              aliquid ex ea commodi consequat. Quis aute iure reprehenderit in
+              voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+              Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui
+              officia deserunt mollit anim id est laborum.
+            </Typography>
+          </Grid>
         </Grid>
       </Card>
     </Flex>
